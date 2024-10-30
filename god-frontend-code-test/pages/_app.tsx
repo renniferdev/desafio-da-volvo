72da650f4a20633f026cfa9bf4a4b383da6817e6
Rennifer Amilton Teixeira <rennifer1-@gmail.com>
2024-10-29 21:32:16 -0300

    Projeto OK

---
+++ god-frontend-code-test/pages/_app.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleProvider, ThemePicker } from "vcc-ui";
+
+import { HomeComponent } from "../src/components/HomeComponent";
+import "../public/css/styles.css";
+
+function HomePage() {
+  return (
+    <React.StrictMode>
+      <StyleProvider>
+        <ThemePicker variant="light">
+          <HomeComponent />
+        </ThemePicker>
+      </StyleProvider>
+    </React.StrictMode>
+  );
+}
+
+export default HomePage;
---
+++ god-frontend-code-test/src/components/Spacer.tsx
@@ -0,0 +1,5 @@
+import styles from "../../public/css/components/spacer.module.css";
+
+export function Spacer() {
+  return <div className={styles.spacer}></div>;
+}
